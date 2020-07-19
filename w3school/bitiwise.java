83707e1807a14011c10d485146e26bfc79691a9b
hiteshshebe <hiteshshebe@gmail.com>
2020-07-20 02:07:15 +0530

    day 7 morning

---
+++ w3school/bitiwise.java
@@ -0,0 +1,13 @@
+class Progaram{
+
+public static void main(String[] args) 
+{
+    byte num=5;
+    int anding=num|1;
+    int noting=~num;
+    System.out.println(anding);
+    System.out.println(noting);
+}
+
+
+}
---
+++ w3school/string.java
@@ -0,0 +1,12 @@
+public class string {
+
+    public static void main(String[] args) {
+        String Alpha = "abcdefghijklmnopqrstuvwxyz";
+        String upper=Alpha.toUpperCase();
+        System.out.println("length of the string are "+Alpha.length());
+        System.out.println(upper);
+        System.out.println(Alpha.indexOf("z"));
+        
+    }
+    
+}
